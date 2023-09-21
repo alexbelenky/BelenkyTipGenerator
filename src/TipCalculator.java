6b37129283984ec0fe3f8e17dfb80a5ce1b99016
BT_2N2_18 <sample@bths.net>
2023-09-21 13:10:16 -0400

    intial commtit

---
+++ src/TipCalculator.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class TipCalculator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Welcome to the tip calculator!");
+        System.out.println("-------------------------------");
+
+    }
+}
