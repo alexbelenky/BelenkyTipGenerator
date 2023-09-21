d25e3a1e2518db055eab16ade852b6ba340d5af4
BT_2N2_18 <sample@bths.net>
2023-09-21 13:15:18 -0400

    just started doing while loop

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -6,6 +6,16 @@ public class TipCalculator {
 
         System.out.println("Welcome to the tip calculator!");
         System.out.println("-------------------------------");
+        System.out.print("How many people feasted on the food? :");
+        int amountPeople = scan.nextInt();
+        scan.nextLine();
+
+        System.out.print("What is the tip percentage? (0-100) :");
+        int tipPercentage = scan.nextInt();
+        scan.nextLine();
+
+
+
 
     }
 }
