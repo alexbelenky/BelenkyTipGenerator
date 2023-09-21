b2f9c3dd223ced5a75cb0032a39403be38d0379a
BT_2N2_18 <sample@bths.net>
2023-09-21 13:22:32 -0400

    finsihed while loop

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -14,8 +14,16 @@ public class TipCalculator {
         int tipPercentage = scan.nextInt();
         scan.nextLine();
 
+        double priceEntered = 0;
+        double finalPrice = 0;
+        while (priceEntered > -1) {
+            System.out.print("Enter the cost of the food item purchased in dollars and cents (-1 to end) : ");
+            priceEntered = scan.nextDouble();
 
-
+            finalPrice += priceEntered;
+        }
+        finalPrice ++;
+        System.out.print(finalPrice);
 
     }
 }
